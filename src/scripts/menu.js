0ed77d62c3c71443fd44deff2f7eb5df0bfa854a
Martin <slackgt@bigpond.com>
2024-07-10 16:03:15 +1000

    6.1

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,7 @@
+console.log("menu.js loaded");
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".main-nav").classList.toggle("active");
-  document.querySelector(".hamburger").classList.toggle("active");
+  document.querySelector(".hamburger").classList.toggle("expanded");
 });
 
 // Set active class on nav when the page is active
@@ -10,9 +11,9 @@ const currentURL = window.location.href;
 navLinks.forEach((link) => {
   if (link.href === currentURL) {
     link.classList.add("active");
-    // link
-    //   .closest("aside")
-    //   .parentElement.querySelector(".drop-icon")
-    //   .classList.add("active");
+    link
+      .closest("aside")
+      .parentElement.querySelector(".drop-icon")
+      .classList.add("active");
   }
 });
